69a38d3d992f2cc443ab585094d512f5e9b27bf7
daniel <d-stoellner@web.de>
2018-04-15 18:20:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,29 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { MessagesComponent } from './messages/messages.component';
+import { FilesComponent } from './files/files.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MessageService} from './messages/message.service';
+import {FileService} from './files/file.service';
+import {SettingsService} from './common/settings.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    FilesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    SettingsService,
+    FileService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/files/files.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FileService} from './file.service';
+import {Tree} from './file';
+import {SettingsService} from '../common/settings.service';
+
+@Component({
+  selector: 'app-files',
+  templateUrl: './files.component.html',
+  styleUrls: ['./files.component.css']
+})
+export class FilesComponent implements OnInit {
+  tree: Tree[];
+  loading: boolean;
+
+  constructor(
+    private fileService: FileService,
+    private settingService: SettingsService
+  ) { }
+
+  ngOnInit() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.loading = true;
+    setTimeout(() => {
+      this.fileService.getFiles().subscribe(tree => this.tree = tree);
+      this.loading = false;
+    }, 1000);
+  }
+}
